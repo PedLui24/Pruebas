0e1ef1de610950448b489972849aa321f720c53c
PedLui24 <160882551+PedLui24@users.noreply.github.com>
2024-02-22 11:28:38 -0400

    Añadiendo la segunda prueba

---
+++ mediaLista.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YourNamespace
+{
+    class YourClass
+    {
+        public static void Main (string[] args)
+        {
+            
+            int sumaTotal = 0;
+            int[] numeros2 = {1, 2, 3, 4, 5};
+            
+            for(int i= 0; i < numeros2.Length; i++){
+                sumaTotal = sumaTotal + numeros2[i];
+            }
+            int res = sumaTotal / numeros2.Length; 
+
+            Console.WriteLine("La Media es " + res);
+        } 
+    }
+
+}
