77794c2c3aa2bfec982bc025c432e645c6e755d4
PedLui24 <160882551+PedLui24@users.noreply.github.com>
2024-02-22 11:28:14 -0400

    Añadiendo la primera prueba

---
+++ fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace YourNamespace
+{
+    class YourClass
+    {
+        public static void Main (string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+
+            Console.Write("Cantidad de Numeros de la Serie Fibonacci: ");
+            int numUser = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numUser; i++){
+                int flag = num1;
+                num1 = num2;
+                num2 = flag + num1;
+                // 0 , 1, 
+                Console.Write(num1+ ", ");
+            }
+        }
+    } 
+}
