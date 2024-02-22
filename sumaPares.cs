7b093fd1341aa1ecc8035a11d767359d82c3b943
PedLui24 <160882551+PedLui24@users.noreply.github.com>
2024-02-22 11:29:30 -0400

    Añadiendo la cuarta prueba

---
+++ sumaPares.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace YourNamespace
+{
+    class YourClass
+    {
+        public static void Main (string[] args)
+        {
+            int total = 0;
+            
+            Console.Write("Cantidad de Numeros Pares: ");
+            int numUser = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= (numUser*2); i++)
+            {
+                if((i%2)==0){
+                    total = total + i;   
+                }
+            }
+
+            Console.WriteLine("Suma total " + total);
+        } 
+    }
+
+}
